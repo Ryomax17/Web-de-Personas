4a3b787726cb8b8b23c826013bc5955ef4f63e42
Ryomax17 <42577059+Ryomax17@users.noreply.github.com>
2023-09-25 20:45:03 -0300

    pre fetch

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+
+async function getPeople(id){
+    const response = await fetch(``)
+    const people = await response.json();
+    console.log(people);
+    return people;
+}
+});
