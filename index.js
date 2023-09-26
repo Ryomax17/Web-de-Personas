a1b02ea4b12172c9f2726f1279995f3a692e1006
Ryomax17 <42577059+Ryomax17@users.noreply.github.com>
2023-09-25 21:59:14 -0300

    boton y fetch

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-document.addEventListener("DOMContentLoaded", function() {
+function alternarTema() {
 
-
-async function getPeople(id){
-    const response = await fetch(``)
-    const people = await response.json();
-    console.log(people);
-    return people;
 }
-});
+async function getPeople() {
+  const response = await fetch(
+    `https://my-json-server.typicode.com/maximiliano836/entrega13/personas`
+  );
+  const peoples = await response.json();
+  console.log(peoples);
+
+  const people = document.getElementById("people");
+  people.innerHTML = "";
+  peoples.forEach((persona) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Nombre: ${persona.name}, Dirección: ${persona.country}, Teléfono: ${persona.phone}`;
+    people.appendChild(listItem);
+  });
+}
+
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle('day-mode');
+    body.classList.toggle('night-mode');
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  
+    getPeople();
+  });
+
